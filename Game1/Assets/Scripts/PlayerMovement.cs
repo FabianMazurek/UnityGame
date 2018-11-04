73f52dd96d8128238b3260b0cafc1b0028e9dcd4
FabianMazurek <fabianmazurek2000@gmail.com>
2018-11-04 11:29:26 +0000

    Added player death, checkpoint system and respawns

--- Game1/Assets/Scripts/PlayerMovement.cs
+++ Game1/Assets/Scripts/PlayerMovement.cs
@@ -6,33 +6,35 @@ public class PlayerMovement : MonoBehaviour
 {
     public float m_Speed;
     public float jumpHeight;
-
+    public float velocity;
+    
     public Transform groundCheck;
     public float groundCheckRadius;
     public LayerMask whatIsGround;
     private bool grounded;
-    
+
 
     // Use this for initialization
     void Start()
     {
-
+        
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
         //Code that checks if the player is on the ground.
-
+        
         grounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, whatIsGround);
     }
     void Update()
     {
         //player movement
-        
+
         if (Input.GetKey(KeyCode.LeftArrow))
         {
             GetComponent<Rigidbody2D>().velocity = new Vector2(-m_Speed, GetComponent<Rigidbody2D>().velocity.y);
+
         }
         if (Input.GetKey(KeyCode.RightArrow))
         {
@@ -45,8 +47,8 @@ public class PlayerMovement : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.UpArrow) && grounded)
         {
             GetComponent<Rigidbody2D>().velocity = new Vector2(GetComponent<Rigidbody2D>().velocity.x, jumpHeight);
-            
+
         }
+
     }
-    
 }
